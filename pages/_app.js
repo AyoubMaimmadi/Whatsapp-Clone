1628f5356592cafd380630039898474eb2f84856
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-25 22:15:53 +0100

    new

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,20 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../firebase'
 import Login from './login'
 import Loading from '../components/Loading'
+import firebase from 'firebase'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
   const [user, loading] = useAuthState(auth)
+
+  useEffect(() => {
+    if (user) {
+      db.collection('users').doc(user.uid).set({
+        email: user.email,
+        lastSee: firebase.firestore.FieldValue.serverTimestamp(),
+      })
+    }
+  }, [user])
 
   if (loading) return <Loading />
   if (!user) return <Login />
