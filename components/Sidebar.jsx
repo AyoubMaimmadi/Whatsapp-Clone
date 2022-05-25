099f7bafa4678a0442f702ff7339e20dc9722e4b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-25 15:35:54 +0100

    new

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components'
+import { Avatar } from '@mui/material'
 
 const Sidebar = () => {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserAvatar />
+      </Header>
     </Container>
   )
 }
@@ -13,3 +16,7 @@ export default Sidebar
 const Container = styled.div``
 
 const Header = styled.div``
+
+const UserAvatar = styled(Avatar)`
+  margin: 10px;
+`
