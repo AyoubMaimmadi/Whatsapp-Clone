0b7c6b2e9a2820e8b44dcb6629b67f7d03cfea19
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-25 17:47:44 +0100

    new

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import Head from 'next/head'
+
+const login = () => {
+  return (
+    <container>
+      <Head>
+        <title>Login</title>
+      </Head>
+    </container>
+  )
+}
+
+export default login
