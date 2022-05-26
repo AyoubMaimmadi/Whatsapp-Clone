74cf5460d9480842e2755a086d87aff1707c8dee
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-26 14:59:45 +0100

    new

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -35,8 +35,29 @@ export default ChatScreen
 
 const Container = styled.div``
 
-const Header = styled.div``
+const Header = styled.div`
+  position: sticky;
+  background-color: white;
+  z-index: 100;
+  top: 0;
+  display: flex;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid whitesmoke;
+`
 
-const HeaderInformation = styled.div``
+const HeaderInformation = styled.div`
+  margin-left: 15px;
+  flex: 1;
+
+  > h3 {
+    margin-bottom: 3px;
+  }
+
+  > p {
+    font-size: 14px;
+    color: gray;
+  }
+`
 
 const HeaderIcons = styled.div``
