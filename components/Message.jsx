3da82f68535135cc7fceebe4ae39ffc77132a80c
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-26 16:50:56 +0100

    new

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,6 +1,10 @@
+import { useAuthState } from 'react-firebase-hooks/auth'
 import styled from 'styled-components'
+import { auth } from '../firebase'
 
 const Message = ({ user, message }) => {
+  const [userLoggedIn] = useAuthState(auth)
+
   return (
     <Container>
       <p>{message.message}</p>
@@ -11,3 +15,14 @@ const Message = ({ user, message }) => {
 export default Message
 
 const Container = styled.div``
+
+const MessageElement = styled.p`
+  width: fit-content;
+  padding: 15px;
+  border-radius: 8px;
+  margin: 10px;
+  min-width: 60px;
+  padding-bottom: 26px;
+  position: relative;
+  text-align: right;
+`
