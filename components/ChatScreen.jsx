4641cf9ec9533bfd76d6f31f3ff3ed2ea62f007c
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-26 14:53:07 +0100

    new

--- components/ChatScreen.jsx
+++ components/ChatScreen.jsx
@@ -1,9 +1,24 @@
 import styled from 'styled-components'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { db, auth } from '../firebase'
+import { useRouter } from 'next/router'
+import { Avatar } from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import AttachFileIcon from '@mui/icons-material/AttachFile'
 
-const ChatScreen = () => {
+const ChatScreen = ({ chat, messages }) => {
+  const [user] = useAuthState(auth)
+  const router = useRouter()
+
   return (
     <Container>
-      <h1>This is a chat</h1>
+      <Header>
+        <Avatar />
+        <HeaderInformation>
+          <h3>Recipient Email</h3>
+          <p>Last seen ...</p>
+        </HeaderInformation>
+      </Header>
     </Container>
   )
 }
@@ -11,3 +26,9 @@ const ChatScreen = () => {
 export default ChatScreen
 
 const Container = styled.div``
+
+const Header = styled.div``
+
+const HeaderInformation = styled.div``
+
+const HeaderIcons = styled.div``
