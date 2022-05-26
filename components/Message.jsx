aa637fd26e83f4716f648ca7afa1e7236a11d701
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-26 17:03:43 +0100

    new

--- components/Message.jsx
+++ components/Message.jsx
@@ -37,3 +37,12 @@ const Receiver = styled(MessageElement)`
   background-color: whitesmoke;
   text-align: left;
 `
+const Timestamp = styled.span`
+  color: gray;
+  padding: 10px;
+  font-size: 9px;
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 0;
+`
